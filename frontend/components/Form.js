64a5ac7389ffd43e7d54e6ff0cf01782479a19be
Katie Ludwig <katierludwig@gmail.com>
2024-04-18 17:45:17 -0700

    saving for now

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as yup from 'yup'
 
-// 👇 Here are the validation errors you will use with Yup.
+// 👇 Here are the VALIDATION errors you will use with Yup.
 const validationErrors = {
   fullNameTooShort: 'full name must be at least 3 characters',
   fullNameTooLong: 'full name must be at most 20 characters',
@@ -8,6 +10,23 @@ const validationErrors = {
 }
 
 // 👇 Here you will create your schema.
+const userSchema = yup.object().shape({
+  fullName: yup.string().trim()
+    .min(3, validationErrors.fullNameTooShort).max(20,validationErrors.fullNameTooLong),
+  size: yup.string()
+    .required(validationErrors.sizeIncorrect)
+    .oneOf(['S', 'M', 'L'], validationErrors.sizeIncorrect),
+})
+
+const getInitialValues = () => ({
+  fullName: '',
+  size: '',
+  toppings: []
+})
+const getInitialErrors = () => ({
+  fullName: '',
+  size: ''
+})
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
@@ -19,43 +38,78 @@ const toppings = [
 ]
 
 export default function Form() {
+const [values, setValues] = useState(getInitialValues());
+const [errors, setErrors] = useState(getInitialErrors());
+const [serverSuccess, setServerSuccess] = useState();
+const [serverFailure, setServerFailure] = useState();
+const [formSubmiting, setFormSubmiting] = useState(false);
+
+useEffect(() => {
+  userSchema.isValid(values).then(setFormSubmiting)
+}, [values])
+
+const onChange = evt => {
+  let { name, value, checked } = evt.target
+  value = name === 'checkbox' ? checked : value
+  setValues({ ...values, [name]: value })
+  yup.reach(userSchema, name).validate(value)
+  .then(() => setErrors({ ...errors, [name]: '' }))
+  .cathc((err) => setErrors({...errors, [name]: err.errors[0]}))
+}
+
+const onSubmit = evt => { 
+  evt.preventDefault()
+  axios.post('http://localhost:9009/api/order', values)
+    .then(res => {
+      setValues(getInitialValues())
+      setServerSuccess(res.data.message)
+      setServerFailure()
+    })
+    .catch(err => {
+      setServerFailure(err.response.data.message)
+      setServerSuccess()
+    })
+}
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {serverSuccess && <div className='success'>{serverSuccess}</div>}
+      {serverFailure && <div className='failure'>{serverFailure}</div>}
 
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input value={values.fullName} onChange={onChange} placeholder="Type full name" id="fullName" type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.fullName && <div className='error'>{errors.fullName} </div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size" value={values.size} onChange={onChange}>
             <option value="">----Choose Size----</option>
             {/* Fill out the missing options */}
+            <option value='S'>Small</option>
+            <option value='M'>Medium</option>
+            <option value='L'>Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.sizeIncorrect && <div className='error'>{errors.sizeIncorrect}</div>}
       </div>
 
       <div className="input-group">
         {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
-        </label>
+        {toppings.map(({ topping_id, text }) => (
+          <div key={topping_id}>
+            <label>
+              <input checked={values.toppings} name={`topping-${topping_id}`} type="checkbox" />{text}
+            </label>
+          </div>
+        ))}
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input disabled={!formSubmiting} type="submit" />
     </form>
   )
 }
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import pizza from './images/pizza.jpg'
+import { useNavigate } from 'react-router-dom'
 
-function Home() {
+export default function Home() {
+  const navigate = useNavigate();
+
+  const imageClick = () => {
+    navigate('/order');
+  }
   return (
     <div>
       <h2>
         Welcome to Bloom Pizza!
       </h2>
       {/* clicking on the img should navigate to "/order" */}
-      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+
+      <img 
+        onClick={imageClick}
+        alt="order-pizza" 
+        style={{ cursor: 'pointer' }} 
+        src={pizza} 
+      />
     </div>
   )
 }
 
-export default Home
+
